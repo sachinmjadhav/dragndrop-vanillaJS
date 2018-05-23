a0de36e47ff99519bf822ff4f805be8edb35e379
Sachin Jadhav <34229084+theSachinJadhav@users.noreply.github.com>
2018-05-23 12:10:06 +0530

    Initial Commit
    Initial Commit

---
+++ js/main.js
@@ -0,0 +1,42 @@
+const fill = document.querySelector(".fill");
+const empties = document.querySelectorAll(".empty");
+
+// Fill listeners
+fill.addEventListener("dragstart", dragStart);
+fill.addEventListener("dragend", dragEnd);
+
+// Loop through empties
+for (const empty of empties) {
+  empty.addEventListener("dragover", dragOver);
+  empty.addEventListener("dragenter", dragEnter);
+  empty.addEventListener("dragleave", dragLeave);
+  empty.addEventListener("drop", dragDrop);
+}
+
+function dragOver(e) {
+  e.preventDefault();
+}
+
+function dragEnter(e) {
+  e.preventDefault();
+  this.className += " hovered";
+}
+
+function dragLeave() {
+  this.className = "empty";
+}
+
+function dragDrop() {
+  this.className = "empty";
+  this.append(fill);
+}
+
+// Drag Functions
+function dragStart() {
+  this.className += " hold";
+  setTimeout(() => (this.className = "invisible"), 0);
+}
+
+function dragEnd() {
+  this.className = "fill";
+}
